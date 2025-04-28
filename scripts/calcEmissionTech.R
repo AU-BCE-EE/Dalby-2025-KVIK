db977c4d33547d1f3f8eb798e39a2a1e9e7ac616
Frederik Rask Dalby <au277187@uni.au.dk>
2025-04-28 11:02:15 +0200

    Revert "update with udbredelse af gødning"
    This reverts commit a834b43acb3f5f15389af38eb5cda27573885324.

--- scripts/calcEmissionTech.R
+++ scripts/calcEmissionTech.R
@@ -141,13 +141,10 @@ for(i in model_gruppe){
 
 out <- out[, ":="(reduktion_totCO2_eq_tot = (totCO2_eq_tot[Scenarie == 'kontrol'] - totCO2_eq_tot) * ((potentiale-udbredelse)/100),
                   reduktion_CO2_eq_tot_m3 = CO2_eq_tot[Scenarie == 'kontrol'] - CO2_eq_tot), by = 'model_gruppe'][order(Scenarie)]
-out[, ":="(TotGoedningabDyr_kt_year_pot = TotGoednabDyr_kt_year * ((potentiale - udbredelse)/100),
-           TotGoedningabDyr_kt_year_udbr = TotGoednabDyr_kt_year * (udbredelse/100))]
+out[, TotGoedningabDyr_kt_year_pot := TotGoednabDyr_kt_year * ((potentiale - udbredelse)/100)]
 out <- out[!duplicated(out[, c('model_gruppe', 'Scenarie')])]
 
-
-out_pot_goedning <- out[Scenarie != 'kontrol', .(TotGoednabDyr_kt_år_pot = sum(TotGoedningabDyr_kt_year_pot),
-                                                 TotGoedningabDyr_kt_år_udbr = sum(TotGoedningabDyr_kt_year_udbr)), by = c('Scenarie', 'Dyr')]
+out_pot_goedning <- out[Scenarie != 'kontrol', .(TotGoednabDyr_kt_år = sum(TotGoedningabDyr_kt_year_pot)), by = c('Scenarie', 'Dyr')]
 write.xlsx(out_pot_goedning, '../output/udbred_poten_kt_Goedn_år.xlsx')
 
 
@@ -187,15 +184,14 @@ units <- data.frame(t(data.frame(units = c('model_gruppe',
            'potent., % af modelgruppe',
            'reduktion total potentiale, kt CO2e pr. år',
            'reduktion, kg CO2e reduceret pr. m3 ab dyr',
-           'Gødning total potentiale, kt ab dyr pr. år',
-           'Gødning total udbredelse, kt ab dyr pr. år'))))
+           'Gødning total potentiale, kt ab dyr pr. år'))))
     
 names(units) <- names(out)       
 out <- rbind(units, out)
 setDT(out)
 
 out_table <- out[, .(model_gruppe, Scenarie, CH4_dyr_stald, CH4_dyr_lager, CH4_dyr_tot, N2O_dyr_tot, 
-                     CO2_eq_fortræng, CO2_eq_tot, TotGoednabDyr_kt_year, TotGoedningabDyr_kt_year_udbr, TotTSabDyr_kt_year, udbredelse, potentiale, 
+                     CO2_eq_fortræng, CO2_eq_tot, TotGoednabDyr_kt_year, TotTSabDyr_kt_year, udbredelse, potentiale, 
                      reduktion_CO2_eq_tot_m3, reduktion_totCO2_eq_tot, Dyr)]
 names(out_table) <- c(t(out_table[1,]))
 unique(out_table$model_gruppe)
@@ -210,8 +206,5 @@ setorder(out_table, Scenarie, model_gruppe)
 
 cols_index <- which(!names(out_table) %in% c('model_gruppe', 'Scenarie','Dyr'))
 out_table[, (cols_index) := lapply(.SD, function(x) round(as.numeric(x), 3)), .SDcols = cols_index]
-
-out_table[, model_gruppe := gsub('_', ' ', model_gruppe)]
-
 
 write.xlsx(out_table, '../output/emis_table_KVIK.xlsx')
