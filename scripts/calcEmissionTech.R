8e6493de0b5e3752ecf9330b7fc444a3f51e5eeb
Frederik Rask Dalby <au277187@uni.au.dk>
2025-04-29 15:18:34 +0200

    add some columns for KVIK table in output files

--- scripts/calcEmissionTech.R
+++ scripts/calcEmissionTech.R
@@ -6,7 +6,6 @@ library(openxlsx)
 
 dat <- fread('../data/dat_merged.csv')
 
-
 # checking amonuts of manure
 #test <- dat[StaldID == 7 & GoedningsNavn == 'Gylle' & Scenarie == 'kontrol']
 #sum(test$TotGoednabDyr)
@@ -84,6 +83,8 @@ dat[grepl('køling', Scenarie) & DyreType == 'søer', strøm_CO2_ton := 1.8]
 
 # Multiply each column in 'cols' by 1 (no change), emis units in kg CH4/m3/year or kg CH4/year, kg CO2 eq/m3/year, kg CO2 eq/year
 # totCO2_eq_tot is in kt CO2 eq emitted (so kg CO2 eq multiplied by 10^6)
+
+# for KVIK25 and onwards we need to exclude strøm used. This was by accident included in KVIK25
 emis <- dat[, (cols) := lapply(.SD, function(x) x * 1), .SDcols = cols, by = c('StaldID', 'Scenarie', 'GoedningsNavn')][
   , (tot_cols) := lapply(.SD, function(x) x * TotGoednabDyr), .SDcols = cols, by = c('StaldID', 'Scenarie')][
     , ":="(CH4_dyr_tot = CH4_dyr_stald + CH4_dyr_lager,
@@ -95,7 +96,7 @@ emis <- dat[, (cols) := lapply(.SD, function(x) x * 1), .SDcols = cols, by = c('
                , ":="(totCH4_dyr_tot = totCH4_dyr_stald + totCH4_dyr_lager,
                       totN2O_dyr_tot = totN2O_dyr_dir_tot + totN2O_dyr_indir_tot)][
                         , totCO2_eq_tot := (totCH4_dyr_tot * ..CO2_eq[['CH4']] + 
-                          totN2O_dyr_tot * ..CO2_eq[['N2O']] + totstrøm_CO2_ton)/1e+06]
+                          totN2O_dyr_tot * ..CO2_eq[['N2O']])/1e+06]
 
 # totCO2_eq_fortræng is in kt CO2 eq (so kg CO2 eq multiplied by 10^6)
 emis[, model_gruppe := 'char']
@@ -115,6 +116,7 @@ emis_summary <- emis[, .(CH4_dyr_stald = unique(CH4_dyr_stald),
                          CH4_dyr_tot = unique(CH4_dyr_tot),
                          N2O_dyr_tot = unique(N2O_dyr_tot),
                          CO2_eq_fortræng = unique(CO2_eq_fortræng),
+                         strøm_CO2_ton = unique(strøm_CO2_ton),
                          CO2_eq_tot = unique(CO2_eq_tot),
                          totCO2_eq_tot = sum(totCO2_eq_tot, na.rm = T)), by = c('Scenarie', 'model_gruppe')]
 
@@ -137,23 +139,27 @@ for(i in model_gruppe){
   }
 }
 
-
-
 out <- out[, ":="(reduktion_totCO2_eq_tot = (totCO2_eq_tot[Scenarie == 'kontrol'] - totCO2_eq_tot) * ((potentiale-udbredelse)/100),
-                  reduktion_CO2_eq_tot_m3 = CO2_eq_tot[Scenarie == 'kontrol'] - CO2_eq_tot), by = 'model_gruppe'][order(Scenarie)]
+                  reduktion_CO2_eq_tot_m3 = CO2_eq_tot[Scenarie == 'kontrol'] - CO2_eq_tot,
+                  reduktion_N2O_tot_m3 = N2O_dyr_tot[Scenarie == 'kontrol'] - N2O_dyr_tot,
+                  reduktion_CH4_tot_m3 = CH4_dyr_tot[Scenarie == 'kontrol'] - CH4_dyr_tot,
+                  reduktion_N2O_CO2eq_tot_m3 = (N2O_dyr_tot[Scenarie == 'kontrol'] - N2O_dyr_tot) * CO2_eq[['N2O']],
+                  reduktion_CH4_CO2eq_tot_m3 = (CH4_dyr_tot[Scenarie == 'kontrol'] - CH4_dyr_tot) *  CO2_eq[['CH4']],
+                  reduktion_strøm_CO2eq_tot_m3 = (strøm_CO2_ton[Scenarie == 'kontrol']- strøm_CO2_ton)), by = 'model_gruppe'][order(Scenarie)]
 out[, ":="(TotGoedningabDyr_kt_year_pot = TotGoednabDyr_kt_year * ((potentiale - udbredelse)/100),
            TotGoedningabDyr_kt_year_udbr = TotGoednabDyr_kt_year * (udbredelse/100))]
 out <- out[!duplicated(out[, c('model_gruppe', 'Scenarie')])]
-
 
 out_pot_goedning <- out[Scenarie != 'kontrol', .(TotGoednabDyr_kt_år_pot = sum(TotGoedningabDyr_kt_year_pot),
                                                  TotGoedningabDyr_kt_år_udbr = sum(TotGoedningabDyr_kt_year_udbr)), by = c('Scenarie', 'Dyr')]
 write.xlsx(out_pot_goedning, '../output/udbred_poten_kt_Goedn_år.xlsx')
 
-
 out_dyr <- copy(out)
 out_dyr <- out_dyr[, .(sum_reduktion_totCO2_eq_tot = sum(reduktion_totCO2_eq_tot),
             mean_reduktion_totCO2_eq_tot_m3 = sum(reduktion_CO2_eq_tot_m3 * TotGoednabDyr_kt_year)/sum(TotGoednabDyr_kt_year),
+            mean_reduktion_CH4_CO2_eq_tot_m3 = sum(reduktion_CH4_CO2eq_tot_m3 * TotGoednabDyr_kt_year)/sum(TotGoednabDyr_kt_year),
+            mean_reduktion_N2O_CO2_eq_tot_m3 = sum(reduktion_N2O_CO2eq_tot_m3 * TotGoednabDyr_kt_year)/sum(TotGoednabDyr_kt_year),
+            mean_reduktion_strøm_CO2_eq_tot_m3 = sum(reduktion_strøm_CO2eq_tot_m3 * TotGoednabDyr_kt_year)/sum(TotGoednabDyr_kt_year),
             CH4_dyr_stald = sum(CH4_dyr_stald * TotGoednabDyr_kt_year)/sum(TotGoednabDyr_kt_year),
             CH4_dyr_lager = sum(CH4_dyr_lager * TotGoednabDyr_kt_year)/sum(TotGoednabDyr_kt_year),
             CH4_dyr_tot = sum(CH4_dyr_tot * TotGoednabDyr_kt_year)/sum(TotGoednabDyr_kt_year),
@@ -189,7 +195,17 @@ units <- data.frame(t(data.frame(units = c('model_gruppe',
            'reduktion, kg CO2e reduceret pr. m3 ab dyr',
            'Gødning total potentiale, kt ab dyr pr. år',
            'Gødning total udbredelse, kt ab dyr pr. år'))))
-    
+
+cols_to_remove <- c(
+  'reduktion_N2O_tot_m3',
+  'reduktion_CH4_tot_m3',
+  'reduktion_N2O_CO2eq_tot_m3',
+  'reduktion_CH4_CO2eq_tot_m3',
+  'reduktion_strøm_CO2eq_tot_m3',
+  'strøm_CO2_ton'
+)
+
+out <- out[, setdiff(names(out), cols_to_remove), with = FALSE]
 names(units) <- names(out)       
 out <- rbind(units, out)
 setDT(out)
@@ -197,6 +213,7 @@ setDT(out)
 out_table <- out[, .(model_gruppe, Scenarie, CH4_dyr_stald, CH4_dyr_lager, CH4_dyr_tot, N2O_dyr_tot, 
                      CO2_eq_fortræng, CO2_eq_tot, TotGoednabDyr_kt_year, TotGoedningabDyr_kt_year_udbr, TotTSabDyr_kt_year, udbredelse, potentiale, 
                      reduktion_CO2_eq_tot_m3, reduktion_totCO2_eq_tot, Dyr)]
+
 names(out_table) <- c(t(out_table[1,]))
 unique(out_table$model_gruppe)
 out_table <- out_table[-1,]
@@ -207,11 +224,8 @@ out_table[, model_gruppe:= factor(model_gruppe, levels = c('kvæg_ringkanal','kv
                                                            'toklimastald_smågrise','spalter_smågrise'))]
                                                            
 setorder(out_table, Scenarie, model_gruppe)
-
 cols_index <- which(!names(out_table) %in% c('model_gruppe', 'Scenarie','Dyr'))
 out_table[, (cols_index) := lapply(.SD, function(x) round(as.numeric(x), 3)), .SDcols = cols_index]
 
 out_table[, model_gruppe := gsub('_', ' ', model_gruppe)]
-
-
 write.xlsx(out_table, '../output/emis_table_KVIK.xlsx')
